d0f4d7d4e24a882f661aff50360b340b630f5c09
Thierry Costa <thierryc.scosta@gmail.com>
2026-05-22 19:10:58 -0300

    arquitetura: criar estrutura de pacotes corporativos e refinar gitignore

---
+++ src/main/java/com/ufs/engdados/EngdadosApplication.java
@@ -0,0 +1,13 @@
+package com.ufs.engdados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngdadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngdadosApplication.class, args);
+	}
+
+}
